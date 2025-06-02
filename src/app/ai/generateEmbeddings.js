8af32d8eafa7622f3320219d23f17dcf906af9be
hchou3 <hchou3@buffalo.edu>
2025-06-02 00:51:22 -0400

    modal

--- src/app/ai/generateEmbeddings.js
+++ src/app/ai/generateEmbeddings.js
@@ -3,6 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const prompt =
   "Transcribe the audio from this video, giving timestamps for salient events in the video. Also provide visual descriptions.";
 
+//Generate a summary of the video content to be used for a vector embedding of the summary.
 export async function summarizeVideo(url) {
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
@@ -14,4 +15,5 @@ export async function summarizeVideo(url) {
       },
     },
   ]);
+  return result.candidates[0].content;
 }
